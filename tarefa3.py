5371cd620e38e699743359a26a7ce3b929293b93
chourizo-byte <pedrodixita20@gmail.com>
2025-02-20 13:51:09 +0100

    Add files via upload

---
+++ tarefa3.py
@@ -0,0 +1,17 @@
+import math
+
+print(math.sqrt(33))
+
+
+alfredoduro= 26.262626
+joseppedrerol=89.898989
+buugatodelvacio=33.33333
+print(math.sqrt(alfredoduro))
+print(math.pow(joseppedrerol,2))
+
+print(math.pi)
+print(round(math.pi))
+
+print(min(alfredoduro,joseppedrerol,buugatodelvacio))
+import random
+print(random.randint(0,3))
