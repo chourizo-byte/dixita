de8cc3b4471040e6d40aedd760eedbe344c4fce8
chourizo-byte <pedrodixita20@gmail.com>
2025-02-20 13:58:08 +0100

    Add files via upload

---
+++ tarefa4.py
@@ -0,0 +1,6 @@
+resp = input("Buuuu cual é o teu nome?   ")
+print("Mira por onde... que nome máis ambicioso " , resp)
+print("Ti es boa people " , resp)
+print("Vales a pena " , resp);
+print("Non hai ninguén máis emocionante ca " , resp)
+print("e... " , resp , ", es a caña!")
