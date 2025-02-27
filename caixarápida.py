b063ffa43b86bed2737284375c574c272972aa60
chourizo-byte <pedrodixita20@gmail.com>
2025-02-27 13:48:52 +0100

    Add files via upload

---
+++ caixarápida.py
@@ -0,0 +1,26 @@
+print('***Supergaviotashop***')
+print('                        ')
+print('                        ')
+
+primeiro=input('introduce o primeiro prezo ')
+print('       ')
+segundo=input('introduce o segundo prezo ')
+print('       ')
+terceiro=input('introduce o terceiro prezo ')
+print('       ')
+total=primeiro + segundo + terceiro
+
+print('______________________________')
+
+print ('a pagar' , total,'$' )
+
+print('______________________________')
+print('                               ')
+print('                               ')
+
+
+
+print('***Supergaviotashop***')
+
+
+
