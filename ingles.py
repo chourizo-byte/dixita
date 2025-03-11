51d32489909703527c720a9f9803e59ff7a7c037
chourizo-byte <pedrodixita20@gmail.com>
2025-03-11 13:50:19 +0100

    Add files via upload

---
+++ ingles.py
@@ -0,0 +1,30 @@
+print('Aprendendo inglés dende o galego')
+casa=input('como se di casa en inglés? ')
+if (casa == 'house'):
+    print('¡Que ben!')
+else:
+    print('hai que mellorar')
+
+gato=input('e gato como se di? ')
+if (gato == 'cat'):
+    print('¡perfect!')
+else:
+    print('hai que estudar algo máis')
+
+can=input('agora dime can')
+if (can == 'dog'):
+    print('very well')
+else:
+    print('very bad')
+
+árbore=input('e agora árbore...  ')
+if (árbore == 'tree'):
+    print('hai te quería eu ver')
+else:
+    print('veña que na seguinte acertas')
+
+coche=input('dime como é coche  ')
+if (coche == 'car'):
+    print('moi ben')
+else:
+    print('na próxima será')
