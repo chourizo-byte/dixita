e31b19c94b2225736f9a2cec0014d54c6b3c6940
chourizo-byte <pedrodixita20@gmail.com>
2025-03-17 16:45:48 +0100

    Add files via upload

---
+++ axenciamatrimonial.py
@@ -0,0 +1,23 @@
+print('Benvidos a túa búsqueda de parella ideal')
+ollos=input('cal é a túa cor de olllos favorita?  ')
+pais=input('de que país che gustan?  ')
+deporte=input('que deporte ques que fagan?  ')
+
+if (ollos == 'negros'):
+    print('o teu home é neymar')
+if (ollos == 'marróns'):
+    print(' estas entre Jordan e Alonso')
+if (pais == 'españa'):
+    print('o teu home é Alonso')
+if (pais ==' EEUU'):
+    print('o teu home é jordan')
+if (pais == 'brasil'):
+    print('o teu home é neymar')
+if (deporte == 'baloncesto'):
+    print('o teu home é jordan')
+if(deporte == 'fútbol'):
+    print(' como está Neymar')
+if (deporte == 'F1'):
+    print('o teu home é magic Alonso')
+if (deporte == 'fútbol'):
+    print('mólache neymar')
