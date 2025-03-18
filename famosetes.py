30d77d545ac0615f44d542170aa5d518acd3ec47
chourizo-byte <pedrodixita20@gmail.com>
2025-03-18 13:52:20 +0100

    Add files via upload

---
+++ famosetes.py
@@ -0,0 +1,34 @@
+print('En que deportista pensas')
+
+deporte=input('Que deporte fai o deportista que estás pensando?  ')
+pelo=input('De que cor é o pelo do deportista que pensas?  ')
+pais=input('De que pais é o deportista que pensas?  ')
+if (deporte == 'ciclismo'):
+    print('o teu deportista é tadej pogaçar')
+elif (deporte == 'baloncesto'):
+    print('LEBROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOON')
+elif (deporte == 'motociclismo'):
+    print('o teu deportista é Dani Pedrosa')
+elif (deporte == 'badminton'):
+    print('a tua deportista é Carolina Marín')
+elif (deporte == 'atletismo'):
+    print('a tua deportista é Ana Peleteiro')
+if (pelo == 'negro'):
+    print('é LeBron James')
+    print('ou Dani Pedrosa')
+    print('tamén pode ser Ana Peleteiro')
+elif (pelo == 'rubio'):
+    print('Tadeeeeeeeeeeeeej Pogaçaaaaaaaaaaaaaar')
+elif (pelo == 'castaño'):
+    print('é Carolina Marín')
+if (pais == 'eslovenia'):
+    print ('é pogaçar')
+elif (pais == 'USA'):
+    print('é Lebron')
+elif (pais == 'españa'):
+    if(deporte == 'motociclismo'):
+        print('o pequeno samurai, Dani Pedrosa')
+    elif(deporte == 'badminton'):
+        print('é Carolina Marín')
+    elif(deporte == 'atletismo'):
+        print('é Ana Peleteiro')
